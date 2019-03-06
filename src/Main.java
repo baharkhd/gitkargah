995e74cf39926809d8fb8ac1c74f23b99b5ff10c
bahar <baharkh127@gmail.com>
2019-03-06 17:00:38 +0330

    changed y in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int x = 6;
-        int y = 8;
-        int z = 9;
-        System.out.println(x + y + z);
+        int y = 10;
+        System.out.println(x + y);
     }
 }
