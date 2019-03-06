fbd16fa73ae4deb9f8f9f4cf25270138c995431b
bahar <baharkh127@gmail.com>
2019-03-06 16:45:59 +0330

    changed main file

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int x = 6;
         int y = 8;
-        System.out.println(x + y);
+        int z = 9;
+        System.out.println(x + y + z);
     }
 }
